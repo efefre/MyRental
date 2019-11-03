4987f7511e14ad080b0c73d4efd4d72a2e77ff4d
Kasia <efefre@icloud.com>
2019-11-03 18:52:19 +0100

    Add settings for django-allauth

--- myRental_root/myRental/settings.py
+++ myRental_root/myRental/settings.py
@@ -37,7 +37,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
+
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
 ]
+
+SITE_ID = 1
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -50,6 +57,11 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'myRental.urls'
+
+AUTHENTICATION_BACKENDS = (
+    'django.contrib.auth.backends.ModelBackend',
+    'allauth.account.auth_backends.AuthenticationBackend',
+)
 
 TEMPLATES = [
     {
