550b126e092b6247c9c65d44e2cec9ee696fab77
Kasia <efefre@icloud.com>
2019-11-07 21:38:27 +0100

    Add view: UserProfile

--- myRental_root/users/views.py
+++ myRental_root/users/views.py
@@ -1,3 +1,17 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
+from django.utils.decorators import method_decorator
+from django.views import View
+
+from .models import UserProfile
 
 # Create your views here.
+@method_decorator(login_required, name='dispatch')
+class UserProfile(View):
+    model = UserProfile
+    template_name = 'users/profile.html'
+
+    def get(self, request, *args, **kwargs):
+        user_detail = self.model.objects.filter(user = request.user)
+        context = {'user_profile' : user_detail}
+        return render(request, self.template_name, context = context)
