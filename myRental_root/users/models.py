cf3f13978f6875bc0f0181a161034bb3a8535bb3
Kasia <efefre@icloud.com>
2019-11-07 20:29:07 +0100

    Add class: UserProfile

--- myRental_root/users/models.py
+++ myRental_root/users/models.py
@@ -1,3 +1,10 @@
+from django.conf import settings
 from django.db import models
 
 # Create your models here.
+class UserProfile(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return f'Profile: {self.user.username}'
