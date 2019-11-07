7fe01c642b1182308e909694a90c95480e383324
Kasia <efefre@icloud.com>
2019-11-07 21:46:25 +0100

    Add first_name and last_name in UserProfile

--- myRental_root/users/models.py
+++ myRental_root/users/models.py
@@ -4,7 +4,8 @@ from django.db import models
 # Create your models here.
 class UserProfile(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    name = models.CharField(max_length=255)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=100)
 
     def __str__(self):
         return f'Profile: {self.user.username}'
