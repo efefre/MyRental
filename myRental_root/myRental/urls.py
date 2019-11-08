c594f54fb9054df3160d42806e2440bebe0867d3
Kasia <efefre@icloud.com>
2019-11-09 00:46:49 +0100

    Add urls for rental app

--- myRental_root/myRental/urls.py
+++ myRental_root/myRental/urls.py
@@ -21,5 +21,6 @@ from users import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
-    path('accounts/profile/', views.UserProfile.as_view(), name='user-profile')
+    path('accounts/profile/', views.UserProfile.as_view(), name='user-profile'),
+    path('rental/', include('rental.urls')),
 ]
