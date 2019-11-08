714cb8577f2fc2a0cb1730149018ea671fe48881
Kasia <efefre@icloud.com>
2019-11-08 21:54:54 +0100

    Add class: Books

--- myRental_root/rental/models.py
+++ myRental_root/rental/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Books(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=200)
+
+    def __str__(self):
+        return f'Book: {self.title}, Author: {self.author}'
