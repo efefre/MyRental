7072f4f57f164a3a233898dd2aa0f8afd07a1f64
Kasia <efefre@icloud.com>
2019-11-09 13:48:58 +0100

    Delete class: Books

--- myRental_root/rental/models.py
+++ myRental_root/rental/models.py
@@ -1,25 +1,3 @@
-from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
-class Books(models.Model):
-    AVAILABLE = 'AV'
-    LOAN = 'LO'
-
-    STATUS_CHOICES = (
-        (AVAILABLE, 'Available'),
-        (LOAN, 'On loan')
-    )
-
-    title = models.CharField(max_length=200)
-    author = models.CharField(max_length=200)
-    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default=AVAILABLE)
-    owner = models.OneToOneField(User, on_delete=models.PROTECT, null=True)
-
-    def __str__(self):
-        return f'Book: {self.title}, Author: {self.author}'
-
-    class Meta:
-        unique_together = ('title', 'author', 'owner',)
-        verbose_name_plural = "Książki"
-        verbose_name = "Książka"
