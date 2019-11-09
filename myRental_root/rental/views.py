e0465f5e663f4fa1564f62a91a6e2eb2c90be30b
Kasia <efefre@icloud.com>
2019-11-09 13:51:30 +0100

    Delete BooksList view

--- myRental_root/rental/views.py
+++ myRental_root/rental/views.py
@@ -1,19 +1,3 @@
-from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
-from django.utils.decorators import method_decorator
-from django.views import View
-from .models import Books
-
 
 # Create your views here.
-@method_decorator(login_required, name='dispatch')
-class BooksList(View):
-    template_name = 'rental/books.html'
-    model = Books
-
-    def get(self, request, *args, **kwargs):
-        user_books = self.model.objects.filter(owner = request.user)
-        context = {
-            'user_books' : user_books
-        }
-        return render(request, self.template_name, context)
