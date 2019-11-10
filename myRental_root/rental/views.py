7e8379edc71aefd8a906500cc2cdbb6fd4167bc9
Kasia <efefre@icloud.com>
2019-11-10 22:47:27 +0100

    Add view: ReturnBookView

--- myRental_root/rental/views.py
+++ myRental_root/rental/views.py
@@ -1,6 +1,9 @@
-from django.views.generic import FormView
+from django.shortcuts import redirect
+from django.urls import reverse
+from django.views.generic import FormView, DeleteView
 from .forms import LoanBookForm
 from books.models import Books
+from .models import LoanBook
 
 # Create your views here.
 class LoanBookView(FormView):
@@ -21,3 +24,22 @@ class LoanBookView(FormView):
         context['book'].save()
         form.save()
         return super().form_valid(form)
+
+class ReturnBookView(DeleteView):
+    model = LoanBook
+    template_name = 'rental/confirm_return.html'
+    context_object_name = 'return'
+
+    def get_object(self, queryset=None):
+        pk = self.kwargs.get('pk')
+        print('Mam pk: ', pk)
+        loan_book_object = LoanBook.objects.get(book_id = pk)
+        return loan_book_object
+
+    def delete(self, request, *args, **kwargs):
+        obj = self.get_object()
+        obj.delete()
+        book = Books.objects.get(pk=self.kwargs.get('pk'))
+        book.status = 'AV'
+        book.save()
+        return redirect(reverse('books:books-list'))
