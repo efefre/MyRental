faa4f2aeb2b574f3cdc0d85c44fe02e77479d8da
Kasia <efefre@icloud.com>
2019-11-10 13:48:44 +0100

    Add book with pk to context

--- myRental_root/rental/views.py
+++ myRental_root/rental/views.py
@@ -1,12 +1,17 @@
-from django.shortcuts import render
 from django.views.generic import FormView
 from .forms import LoanBookForm
+from books.models import Books
 
 # Create your views here.
 class LoanBookView(FormView):
     template_name = 'rental/loan_book.html'
     form_class = LoanBookForm
     success_url = '/books'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['book'] = Books.objects.get(pk=self.kwargs.get('pk'))
+        return context
 
     def form_valid(self, form):
         form.save()
