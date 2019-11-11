6f409accf3de90bd4b2292ee80c83140ff744f78
Kasia <efefre@icloud.com>
2019-11-11 14:55:03 +0100

    Add LoanDetailContextMixin

--- myRental_root/books/views.py
+++ myRental_root/books/views.py
@@ -2,10 +2,21 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 from django.utils.decorators import method_decorator
 from django.views import View
+from django.views.generic.base import ContextMixin
+
 from .models import Books
+from rental.models import LoanBook
 
 
 # Create your views here.
+class LoanDetailContextMixin(ContextMixin):
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        loan_book = LoanBook.objects.all()
+        context = loan_book
+        return context
+
+
 @method_decorator(login_required, name='dispatch')
 class BooksList(View):
     template_name = 'books/books.html'
