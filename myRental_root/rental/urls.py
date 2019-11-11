dacd97d8f6a44f6962510b25a9748e60e978ba61
Kasia <efefre@icloud.com>
2019-11-11 13:06:50 +0100

    Add url: book-not-available

--- myRental_root/rental/urls.py
+++ myRental_root/rental/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import LoanBookView, ReturnBookView
+from .views import LoanBookView, ReturnBookView, BookNotAvailable
 
 app_name = 'rental'
 
 urlpatterns = [
     path('loan-book/<int:pk>/', LoanBookView.as_view(), name='loan-book'),
     path('return-book/<int:pk>/', ReturnBookView.as_view(), name='return-book'),
+    path('book-not-available/<int:pk>/', BookNotAvailable.as_view(), name='book-not-available'),
     ]
