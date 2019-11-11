e2ae380969eb15caeeccbe6924fe297d7ff99902
Kasia <efefre@icloud.com>
2019-11-11 20:19:14 +0100

    Add url for AddBookView

--- myRental_root/books/urls.py
+++ myRental_root/books/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import BooksList
+from .views import BooksList, AddBookView
 
 app_name = 'books'
 
 urlpatterns = [
     path('', BooksList.as_view(), name='books-list'),
+    path('add-book/', AddBookView.as_view(), name='add-book'),
     ]
