ddebea3f9898c0813ffde1f3b195bb970eccdbe1
Kasia <efefre@icloud.com>
2019-11-27 23:00:11 +0100

    Check if book exist in user book list

--- myRental_root/books/forms.py
+++ myRental_root/books/forms.py
@@ -1,4 +1,6 @@
 from django import forms
+from django.core.exceptions import ValidationError
+
 from books.models import Books
 
 
@@ -11,6 +13,19 @@ class AddBookForm(forms.ModelForm):
     class Meta:
         model = Books
         exclude = ['owner', 'status']
+
+    def __init__(self, *args, **kwargs):
+        self.request = kwargs.pop('request', None)
+        super(AddBookForm, self).__init__(*args, **kwargs)
+
+    def clean(self):
+        cleaned_data = self.cleaned_data
+        if Books.objects.filter(title=cleaned_data['title'],
+                                author=cleaned_data['author'],
+                                owner = self.request.user).exists():
+            raise ValidationError('Książka już istnieje')
+
+        return cleaned_data
 
 
 class UpdateBookForm(forms.ModelForm):
