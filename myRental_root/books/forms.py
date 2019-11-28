d2d2883c5e16aed682c103d0a1a3557e7f85c4dd
Kasia <efefre@icloud.com>
2019-11-28 18:46:03 +0100

    Update __init__ in AddBookForm

--- myRental_root/books/forms.py
+++ myRental_root/books/forms.py
@@ -6,6 +6,7 @@ from books.models import Books
 
 class AddBookForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
+        self.request = kwargs.pop('request', None)
         super().__init__(*args, **kwargs)
         self.fields['author'].widget.attrs.update({'class': 'form-control'})
         self.fields['title'].widget.attrs.update({'class': 'form-control'})
@@ -13,10 +14,6 @@ class AddBookForm(forms.ModelForm):
     class Meta:
         model = Books
         exclude = ['owner', 'status']
-
-    def __init__(self, *args, **kwargs):
-        self.request = kwargs.pop('request', None)
-        super(AddBookForm, self).__init__(*args, **kwargs)
 
     def clean(self):
         cleaned_data = self.cleaned_data
