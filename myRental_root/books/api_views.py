06bd6a1b22bc2828e2271877eea25a777e563d05
Kasia <efefre@icloud.com>
2019-11-29 18:27:22 +0100

    Add permission_classes in BooksViewset

--- myRental_root/books/api_views.py
+++ myRental_root/books/api_views.py
@@ -1,7 +1,10 @@
 from rest_framework import viewsets
+
+from books.permissions import IsOwner
 from . import models
 from . import serializers
 
 class BooksViewset(viewsets.ModelViewSet):
     queryset = models.Books.objects.all()
     serializer_class = serializers.BooksSerializer
+    permission_classes = [IsOwner]
