6950825d20d386ff037bd0dc52635b962ca305f5
Kasia <efefre@icloud.com>
2019-12-27 18:53:14 +0100

    Add basename in router.register

--- myRental_root/myRental/api.py
+++ myRental_root/myRental/api.py
@@ -2,4 +2,4 @@ from rest_framework import routers
 from books import api_views as books_views
 
 router = routers.DefaultRouter()
-router.register(r'books', books_views.BooksViewset)
+router.register(r'books', books_views.BooksViewset, basename='Books')
