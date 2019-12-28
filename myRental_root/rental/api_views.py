ad37aae6f94052c394d823d51522181ff2479401
Kasia <efefre@icloud.com>
2019-12-28 16:53:58 +0100

    Delete queryset from LoanBookViewset

--- myRental_root/rental/api_views.py
+++ myRental_root/rental/api_views.py
@@ -5,7 +5,6 @@ from . import serializers
 
 class LoanBookViewset(viewsets.ModelViewSet):
     serializer_class = serializers.LoanBookSerializer
-    queryset = models.LoanBook.objects.all()
 
     def get_queryset(self):
         queryset = models.LoanBook.objects.filter(book__owner = self.request.user)
