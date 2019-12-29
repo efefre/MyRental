5ad38ddc6d32d93661969692977c7c36d7ea9162
Kasia <efefre@icloud.com>
2019-12-29 14:02:14 +0100

    Add validator for book field. Check if the user is the book's owner

--- myRental_root/rental/serializers.py
+++ myRental_root/rental/serializers.py
@@ -2,6 +2,16 @@ from rest_framework import serializers
 from . import models
 
 class LoanBookSerializer(serializers.ModelSerializer):
+
     class Meta:
         model = models.LoanBook
         fields = '__all__'
+
+    def validate_book(self, value):
+        if self.context['request']:
+            user = self.context['request'].user
+
+        if value.owner != user:
+            raise serializers.ValidationError('You are not an owner of this book.')
+
+        return value
