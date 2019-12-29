568a7e82e36bd4fc7dcbf807e440ba732785d14b
Kasia <efefre@icloud.com>
2019-12-29 14:47:33 +0100

    Add custom method: destroy in LoanBookViewset. Update book status

--- myRental_root/rental/api_views.py
+++ myRental_root/rental/api_views.py
@@ -1,6 +1,9 @@
+from rest_framework.response import Response
+from rest_framework import status
 from rest_framework import viewsets
 
 from . import models
+from books import models as books_models
 from . import serializers
 
 class LoanBookViewset(viewsets.ModelViewSet):
@@ -9,3 +12,12 @@ class LoanBookViewset(viewsets.ModelViewSet):
     def get_queryset(self):
         queryset = models.LoanBook.objects.filter(book__owner = self.request.user)
         return queryset
+
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        book_id = instance.book.id
+        self.perform_destroy(instance)
+
+        books_models.Books.objects.filter(id=book_id).update(status='AV')
+
+        return Response(status=status.HTTP_204_NO_CONTENT)
