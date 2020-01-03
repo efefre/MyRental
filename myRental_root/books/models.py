2a1768a2b2fa5febb81866fad9372c74d80a8f64
Kasia <efefre@icloud.com>
2020-01-03 23:33:06 +0100

    Delete status from model: Book

--- myRental_root/books/models.py
+++ myRental_root/books/models.py
@@ -3,17 +3,8 @@ from django.db import models
 
 # Create your models here.
 class Books(models.Model):
-    AVAILABLE = 'AV'
-    LOAN = 'LO'
-
-    STATUS_CHOICES = (
-        (AVAILABLE, 'Dostępna'),
-        (LOAN, 'Wypożyczona')
-    )
-
     title = models.CharField(verbose_name='Tytuł', max_length=200)
     author = models.CharField(verbose_name='Autor', max_length=200)
-    status = models.CharField(verbose_name='Status',max_length=20, choices=STATUS_CHOICES, default=AVAILABLE)
     owner = models.ForeignKey(User, on_delete=models.PROTECT, null=True)
 
     def __str__(self):
